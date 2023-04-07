ec2f20e55c8ef8a9076fe3b44183f45972dd979b
Ralph King Jr <15459559+kingralph33@users.noreply.github.com>
2023-04-06 23:53:39 -0400

    saving work

--- src/Components/Square/index.tsx
+++ src/Components/Square/index.tsx
@@ -1,7 +1,18 @@
+import { useState } from 'react';
 import './Square.css';
 
-function Square({ value }: { value: string }) {
-	return <button className='square'>{value}</button>;
+function Square({
+	value,
+	onSquareClick,
+}: {
+	value: string;
+	onSquareClick: string;
+}) {
+	return (
+		<button className='square' onClick={onSquareClick}>
+			{value}
+		</button>
+	);
 }
 
 export default Square;
